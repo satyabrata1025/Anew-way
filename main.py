4ef7a4c8d6d8f702d97e53ae96035e46c946bff1
SATYABRATA  JENA <143281006+satyabrata1025@users.noreply.github.com>
2023-12-14 21:52:53 +0530

    Add files via upload
    NEW main.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from taipy import Gui
-value= "stock simulator by satya"
+value = "stock simulator by satya"
+path = "logo.jpeg"
+company_name ="TATA"
+company_minprice= 200
+company_maxprice= 2000
+
+
 page="""
 <|text-center |
+<|{path}|image|>
+
 <|{value}|hover_text =welcome to the club|>
+
+
+Name of stock:<|{company_name}|input|>
+
+Name of min_price:<|{company_minprice}|input|>
+
+Name of max_price:<|{company_maxprice}|input|>
+
+
+
 >
- 
  
 """
 if __name__ == "__main__":
